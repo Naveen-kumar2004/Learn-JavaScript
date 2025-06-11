e00691601b83c1e95e992dcd3395d16a92031ada
Naveen-kumar2004 <142926198+Naveen-kumar2004@users.noreply.github.com>
2025-06-11 23:28:49 +0530

    done till object spreading

--- Learn Javascript/object literals.js
+++ Learn Javascript/object literals.js
@@ -11,11 +11,36 @@ const person={
 
 };
 
+
 x=person.name;
 x=person['age']
-//x=person.addresss.city;
-
-
-
-
 console.log(x);
+//x=person.addresss.city;
+let pn=person.addresss.pincode;
+console.log("pin code : "+pn);
+
+//deleting age
+delete person.age;
+
+x=person;
+console.log(x);
+//check it has children
+ b=person.hasChildren=true;
+console.log(b);
+
+//using function 
+person.greet = function(){
+    console.log(`hello my name is ${this.name} `);
+};
+person.greet();
+
+const person={
+    
+}
+
+
+
+
+
+
+
---
+++ Learn Javascript/object spread oprator.js
@@ -0,0 +1,24 @@
+let x;
+
+const todo= new Object();
+todo.id=1;
+todo.name="milk";
+todo.completed=false;
+
+// object spreading 
+const obj1={a:1,b:2};
+const obj2={c:3,d:4};
+const obj3={...obj1,...obj2};
+//using object assign
+const obj4=Object.assign({},obj1,obj2);
+
+console.log(obj3);
+console.log("using assign : "+obj4);
+
+//creating a constructor
+
+
+
+x=todo;
+console.log(x);
+
