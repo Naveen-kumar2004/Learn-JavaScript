7ba468729fbd3af8f920738a8601f6d7ab64e1d8
Naveen-kumar2004 <142926198+Naveen-kumar2004@users.noreply.github.com>
2025-06-11 19:29:44 +0530

    Add files via upload

---
+++ Learn Javascript/object literals.js
@@ -0,0 +1,20 @@
+const person={
+    name:"naveen",
+    age:18,
+    addresss:{
+        street:"TVS ",
+        city:"palladam",
+        Distrct:"itirupur",
+
+    }
+
+};
+
+x=person.name;
+x=person['age']
+//x=person.addresss.city;
+
+
+
+
+console.log(x);
