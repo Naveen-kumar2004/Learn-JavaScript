f519e6772bf24ea6282e9d3545895ff62c95dcd1
Naveen-kumar2004 <142926198+Naveen-kumar2004@users.noreply.github.com>
2025-06-12 15:47:47 +0530

    till json

---
+++ Learn Javascript/JSON.js
@@ -0,0 +1,38 @@
+ const post={
+    id:1,
+    title:"Post",
+    body:"this the post body",
+
+ };
+ //convert objects to json string
+const str=JSON.stringify(post);
+
+//parse JSOn
+const obj=JSON.parse(str);
+//for array
+const posts=[{
+    id:1,
+    title:"Post one",
+    body:"this the post 1 body",
+
+ },
+ {
+    id:2,
+    title:"Post two",
+    body:"this the post 2 body",
+ },
+];
+const str2=JSON.stringify(posts);  // converting to json string
+
+
+
+
+
+console.log(post);
+console.log("object to json string :",str);
+console.log("parsing : ",str);
+console.log("array obj is: ",str2);
+
+console.log(str.id); // undefined
+console.log(obj.id);
+
