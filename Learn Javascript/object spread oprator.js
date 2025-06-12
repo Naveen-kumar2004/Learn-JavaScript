c33bcb9fd2849ab0dc4eeaadd80b12ccb580d4e5
Naveen-kumar2004 <142926198+Naveen-kumar2004@users.noreply.github.com>
2025-06-12 12:43:07 +0530

    complted till deconstruction of object and array

--- Learn Javascript/object spread oprator.js
+++ Learn Javascript/object spread oprator.js
@@ -1,11 +1,9 @@
 let x;
 
-// const todo= new Object();
-// todo.id=1;
-// todo.name="milk";
-// todo.completed=false;
-
-//
+const todo= new Object();
+todo.id=1;
+todo.name="milk";
+todo.completed=false;
 
 // object spreading 
 const obj1={a:1,b:2};
@@ -19,8 +17,35 @@ console.log("using assign : "+obj4);
 
 //creating a constructor
 
-
-
 x=todo;
 console.log(x);
+
+
+//
+
+let todos=[
+    {id:1,name:"nk"},
+    {id:2,name:"go to work"},
+    {id:3,name:"learn"},
+]
+x=todos;
+x=todos[0];
+console.log("todo is : ",todo);
+let y=Object.keys(todo);   ///keys
+let nmuofd=Object.keys(todo).length;  // no of datas
+let v=Object.values(todo);       // values alone
+let e=Object.entries(todo);
+let p=Object.hasOwnProperty("age");    //checking
+
+
+console.log(x);
+console.log(y);
+console.log("total datasin object : ",nmuofd);
+console.log(v);
+console.log(e);
+console.log(p);
+
+
+
+
 
